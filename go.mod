bf20efc0e45db92d0c1ca3c924c8176b5a0b7d74
Andrei Barbu <and.barbu@gmail.com>
2022-10-04 11:14:06 -0700

    fakes

--- go.mod
+++ go.mod
@@ -3,47 +3,48 @@ module github.com/AndreiBarbuOz/playground
 go 1.18
 
 require (
-	github.com/distribution/distribution/v3 v3.0.0-20220608161812-0eca21129409
-	github.com/sirupsen/logrus v1.8.1 // indirect
+	k8s.io/api v0.25.2
+	k8s.io/apimachinery v0.25.2
+	k8s.io/client-go v0.25.2
 )
 
 require (
-	github.com/phayes/freeport v0.0.0-20220201140144-74d24b5ae9f5
-	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
-)
-
-require (
-	github.com/Shopify/logrus-bugsnag v0.0.0-20171204204709-577dee27f20d // indirect
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/bshuster-repo/logrus-logstash-hook v1.0.0 // indirect
-	github.com/bugsnag/bugsnag-go v0.0.0-20141110184014-b1d153021fcd // indirect
-	github.com/bugsnag/osext v0.0.0-20130617224835-0dd3f918b21b // indirect
-	github.com/bugsnag/panicwrap v0.0.0-20151223152923-e2c28503fcd0 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/docker/go-events v0.0.0-20190806004212-e31b211e4f1c // indirect
-	github.com/docker/go-metrics v0.0.1 // indirect
-	github.com/docker/libtrust v0.0.0-20150114040149-fa567046d9b1 // indirect
-	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/emicklei/go-restful/v3 v3.8.0 // indirect
+	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.5 // indirect
+	github.com/go-openapi/swag v0.19.14 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/gomodule/redigo v1.8.2 // indirect
-	github.com/gorilla/handlers v1.5.1 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/opencontainers/image-spec v1.0.2 // indirect
-	github.com/prometheus/client_golang v1.12.1 // indirect
-	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.32.1 // indirect
-	github.com/prometheus/procfs v0.7.3 // indirect
-	github.com/spf13/cobra v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.3 // indirect
-	github.com/yvasiyarov/go-metrics v0.0.0-20140926110328-57bccd1ccd43 // indirect
-	github.com/yvasiyarov/gorelic v0.0.0-20141212073537-a9bba5b9ab50 // indirect
-	github.com/yvasiyarov/newrelic_platform_go v0.0.0-20140908184405-b21fdbd4370f // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	github.com/google/gnostic v0.5.7-v3refs // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/klog/v2 v2.80.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20220803162953-67bda5d908f1 // indirect
+	k8s.io/utils v0.0.0-20220922133306-665eaaec4324 // indirect
+	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,166 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	_ "github.com/distribution/distribution/v3/registry/auth/htpasswd"           // used for docker test registry
-	_ "github.com/distribution/distribution/v3/registry/storage/driver/inmemory" // used for docker test registry
-	"io/ioutil"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kuberuntime "k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes/fake"
+	clienttesting "k8s.io/client-go/testing"
+	"k8s.io/client-go/tools/cache"
+	watchtools "k8s.io/client-go/tools/watch"
+	"time"
 )
 
+const (
+	namespace = "default"
+)
+
+func podRunningAndReady(event watch.Event) (bool, error) {
+	switch event.Type {
+	case watch.Deleted:
+		return false, fmt.Errorf("pod already deleted")
+	}
+	switch t := event.Object.(type) {
+	case *corev1.Pod:
+		switch t.Status.Phase {
+		case corev1.PodFailed, corev1.PodSucceeded:
+			return false, fmt.Errorf("pod %s/%s already completed", t.Namespace, t.Name)
+		case corev1.PodRunning:
+			conditions := t.Status.Conditions
+			if conditions == nil {
+				return false, nil
+			}
+			for i := range conditions {
+				if conditions[i].Type == corev1.PodReady &&
+					conditions[i].Status == corev1.ConditionTrue {
+					return true, nil
+				}
+			}
+		}
+	}
+	return false, nil
+}
+
+// waitForPod watches the given pod until the exitCondition is true
+func waitForPod(timeout time.Duration) (*corev1.Pod, error) {
+	ctx, cancel := watchtools.ContextWithOptionalTimeout(context.Background(), timeout)
+	defer cancel()
+
+	client := fake.NewSimpleClientset()
+	podClient := client.CoreV1()
+	//labelSelector, err := labels.NewRequirement("app", selection.Equals, []string{"istioValidation"})
+	fmt.Printf("creating the ListWatch\n")
+	lw := &cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (kuberuntime.Object, error) {
+			//options.LabelSelector = labelSelector.String()
+			return podClient.Pods(namespace).List(context.TODO(), options)
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			//options.LabelSelector = labelSelector.String()
+			return podClient.Pods(namespace).Watch(context.TODO(), options)
+		},
+	}
+
+	var result *corev1.Pod
+	fmt.Printf("starting the watch\n")
+	ev, err := watchtools.UntilWithSync(ctx, lw, &corev1.Pod{}, nil, podRunningAndReady)
+	if ev != nil {
+		result = ev.Object.(*corev1.Pod)
+	}
+
+	return result, err
+}
+
 func main() {
-	body, _ := ioutil.ReadFile("2021.10.json")
-	tags, _ := unmarshallUnstructured(body)
-	fmt.Printf("%v\n", tags)
+	var ()
+	watcherStarted := make(chan struct{})
+	// Create the fake client.
+	client := fake.NewSimpleClientset()
 
-	tags, _ = getTypedTagList(body)
-	fmt.Printf("%v\n", tags)
+	// A catch-all watch reactor that allows us to inject the watcherStarted channel.
+	client.PrependWatchReactor("*", func(action clienttesting.Action) (handled bool, ret watch.Interface, err error) {
+		gvr := action.GetResource()
+		ns := action.GetNamespace()
+		fmt.Printf("starting watch\n")
+		watch, err := client.Tracker().Watch(gvr, ns)
+		if err != nil {
+			return false, nil, err
+		}
+		close(watcherStarted)
+		return true, watch, nil
+	})
+	client.PrependReactor("list", "pods", func(action clienttesting.Action) (handled bool, ret kuberuntime.Object, err error) {
 
+	} func(action clienttesting.Action) (handled bool, ret watch.Interface, err error) {
+		gvr := action.GetResource()
+		ns := action.GetNamespace()
+		fmt.Printf("starting watch\n")
+		watch, err := client.Tracker().Watch(gvr, ns)
+		if err != nil {
+			return false, nil, err
+		}
+		close(watcherStarted)
+		return true, watch, nil
+	})
+
+	pods := make(chan *corev1.Pod, 1)
+
+	go func() {
+		pod, err := waitForPod(100 * time.Second)
+		fmt.Printf("error: %v\tpod: %v\n", err, pod)
+		pods <- pod
+	}()
+
+	//// We will create an informer that writes added pods to a channel.
+	//informers := informers.NewSharedInformerFactory(client, 0)
+	//podInformer := informers.Core().V1().Pods().Informer()
+	//podInformer.AddEventHandler(&cache.ResourceEventHandlerFuncs{
+	//	AddFunc: func(obj interface{}) {
+	//		pod := obj.(*corev1.Pod)
+	//		t.Logf("pod added: %s/%s", pod.Namespace, pod.Name)
+	//		pods <- pod
+	//	},
+	//})
+	//
+	//// Make sure informers are running.
+	//informers.Start(ctx.Done())
+	//
+	//// This is not required in tests, but it serves as a proof-of-concept by
+	//// ensuring that the informer goroutine have warmed up and called List before
+	//// we send any events to it.
+	//cache.WaitForCacheSync(ctx.Done(), podInformer.HasSynced)
+
+	// The fake client doesn't support resource version. Any writes to the client
+	// after the informer's initial LIST and before the informer establishing the
+	// watcher will be missed by the informer. Therefore we wait until the watcher
+	// starts.
+	// Note that the fake client isn't designed to work with informer. It
+	// doesn't support resource version. It's encouraged to use a real client
+	// in an integration/E2E test if you need to test complex behavior with
+	// informer/controllers.
+	<-watcherStarted
+	fmt.Printf("watcher started\n")
+	// Inject an event into the fake client.
+	p := &corev1.Pod{ObjectMeta: metav1.ObjectMeta{
+		Name: "my-pod",
+		Labels: map[string]string{
+			"app": "istioValidation",
+		},
+	}}
+	_, err := client.CoreV1().Pods("istio-system").Create(context.TODO(), p, metav1.CreateOptions{})
+	if err != nil {
+		panic(fmt.Errorf("error creating pod: %v\n", err))
+	}
+	fmt.Println("created pod")
+
+	select {
+	case pod := <-pods:
+		fmt.Printf("Got pod from channel: %s/%s\n", pod.Namespace, pod.Name)
+	case <-time.After(wait.ForeverTestTimeout):
+		panic("Informer did not get the added pod")
+	}
 }
